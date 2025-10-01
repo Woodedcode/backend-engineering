042658956bbb9a7dc29bcdcb5f0ef917c0674c32
Ryan <Woodedcode@gmail.com>
2025-09-30 22:29:41 -0500

    Add files via upload

---
+++ software-engineering/function-citizen.js
@@ -0,0 +1,60 @@
+// const greet = (name) => {
+//     console.log('greeting...')
+//     return 'hello ' + name
+// };
+
+// console.log(greet('Ryan'))
+
+
+// const functions = [
+//     (x) => x * 2,
+//     (x) => x * x,
+//     (x) => x / 2
+// ]
+
+// console.log(functions[0](5))
+// console.log(functions[1](5))
+// console.log(functions[2](5))
+// const functions = {
+//     double: (x) => x * 2,
+//     square: (x) => x * x,
+//     half: (x) => x / 2
+// }
+
+// console.log(functions.half(5))
+
+// HASH FUNCTION
+
+const crypto = require('crypto');
+
+function sha256(data) {
+    if(!sha256.cache){
+        console.log('initializing cache')
+        sha256.cache = {}
+    }
+
+    
+    if(!sha256.cache[data]){
+    const hash = crypto.createHash('sha256')
+    hash.update(data);
+    sha256.cache[data] = hash.digest('hex')
+    } else {
+        console.log('cache hit!')
+    }
+
+
+
+    return sha256.cache[data]
+}
+
+console.log(sha256('hello world'));
+
+
+
+
+
+
+
+
+
+
